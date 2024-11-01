22b56febbd1f907718f75829b0d54a43514d7048
Dennis Moldovan <dennismoldovan32@gmail.com>
2024-11-01 13:13:16 +0200

    chore - hero

---
+++ app/(root)/page.tsx
@@ -0,0 +1,17 @@
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({searchParams}: {searchParams: Promise<{query?: string}>}) {
+  
+  const query = (await searchParams).query;
+
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">Pitch Your Startup, <br /> Connect with Entreprenurs</h1>
+        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and get Noticed in Virtual Competitions</p>
+      
+        <SearchForm query={query} />
+      </section>
+    </>
+  );
+}
