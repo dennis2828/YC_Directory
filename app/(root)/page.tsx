891c5675fb7757892f4c2bdff58d0c942d7de7d4
Dennis Moldovan <dennismoldovan32@gmail.com>
2024-11-06 19:00:41 +0200

    chore - layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,9 +7,9 @@ export default async function Home({searchParams}: {searchParams: Promise<{query
   return (
     <>
       <section className="pink_container">
-        <h1 className="heading">Pitch Your Startup, <br /> Connect with Entreprenurs</h1>
+        <h1 className="heading">Pitch Your Startup,  <br /> Connect with Entreprenurs</h1>
         <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and get Noticed in Virtual Competitions</p>
-      
+        
         <SearchForm query={query} />
       </section>
     </>
